fc5a9cab15b0ab89cbcfbd200d2be1e5d6ccb1b7
Ashish Naidu <naidua@avaya.com>
2021-09-05 01:37:44 +0530

    Added logs

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,12 +1,16 @@
 package com.javatpoint.controller;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "Hello javaTpoint";
-}
+public class HelloWorldController {
+	@RequestMapping("/")
+	public String hello() {
+		Logger.getAnonymousLogger().log(Level.INFO, "Hi$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		return "Hello javaTpoint";
+	}
 }
